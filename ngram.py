3300c0e217c766f0a73cb439f518a67a38badae1
Jon Pfeil <piffle21@gmail.com>
2015-09-21 22:37:57 -0400

    Added ngram counter to count occurences in corpus

---
+++ ngram.py
@@ -0,0 +1,44 @@
+import os, pickle
+from collections import defaultdict
+from preprocessing import CorpusBuilder
+from itertools import tee
+
+def window(iterable, size):
+    iters = tee(iterable, size)
+    for i in range(1, size):
+        for each in iters[i:]:
+            next(each, None)
+    return zip(*iters)
+
+class NGramCounts(object):
+    def __init__(self, n):
+        self.n = n
+        self.corpus_builder = CorpusBuilder()
+        self.corpus = self.corpus_builder.load_corpus()
+        self.counts = defaultdict(lambda: defaultdict(int))
+        # set count data
+        self.load_counts()
+
+    def build_counts(self):
+        for i in range(1, self.n + 1):
+            self.counts[i] = self._build_counts(i)
+        pickle.dump(dict(self.counts)   , open(self.filename(), 'wb'))
+
+    def _build_counts(self, n):
+        counts = defaultdict(int)
+        for s in self.corpus:
+            s = ["START"] + s + ["END"]
+            for n_gram in window(s, n):
+                counts[tuple(n_gram)] += 1
+        return counts
+
+    def load_counts(self, update=False):
+        if update or not os.path.exists(self.filename()):
+            self.build_counts()
+        self.counts = pickle.load(open(self.filename(), 'rb'))
+
+    def filename(self):
+        return '%s/%dgram_counts.pickle' % (self.corpus_builder.data_path, self.n)
+
+if __name__ == '__main__':
+    NGramCounts(3).load_counts()
